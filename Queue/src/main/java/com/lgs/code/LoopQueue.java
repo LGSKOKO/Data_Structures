111f6eb616c6d650d7a8e7b02ea5858010271187
LGSKOKO <420422844@qq.com>
2019-08-12 23:03:24 +0800

    循环队列

--- Queue/src/main/java/com/lgs/code/LoopQueue.java
+++ Queue/src/main/java/com/lgs/code/LoopQueue.java
@@ -14,72 +14,79 @@ import java.util.Arrays;
 public class LoopQueue<E> implements Queue<E> {
 
     private E[] data;
-    private int front,tail;//头指针 尾指针
+    private int front, tail;//头指针 尾指针
     private int size;
 
-    //有参构造函数
     public LoopQueue(int capacity){
-        data = (E[])new Object[capacity+1];
+        data = (E[])new Object[capacity + 1];
+        front = 0;
+        tail = 0;
+        size = 0;
     }
 
-    //无参构造函数 代码复用
     public LoopQueue(){
         this(10);
     }
+
     public int getCapacity(){
-        return data.length-1;
-    }
-    @Override
-    public int getSize() {
-        return size;
+        return data.length - 1;
     }
 
     @Override
-    public boolean isEmpty() {
+    public boolean isEmpty(){
         return front == tail;
     }
 
+    @Override
+    public int getSize(){
+        return size;
+    }
+
     public void enqueue(E e){
-        if((tail+1)%data.length == front)
-            resize(getCapacity()*2);//进行扩容操作
-         data[tail] = e;
-         tail = (tail+1) % data.length;
-         size++;
+
+        if((tail + 1) % data.length == front)
+            resize(getCapacity() * 2);
+
+        data[tail] = e;
+        tail = (tail + 1) % data.length;
+        size ++;
     }
 
     @Override
-    public E dequeue() {
+    public E dequeue(){
+
         if(isEmpty())
-            throw new IllegalArgumentException("The LoopQueue is empty");
+            throw new IllegalArgumentException("Cannot dequeue from an empty queue.");
+
         E ret = data[front];
         data[front] = null;
-        front = (front+1)% data.length;//重新指定front大小
-        size--;//当前队列元素个数减一
-        if(size == getCapacity()/4 && getCapacity()/2 !=0)
-            resize(getCapacity()/2);//进行缩容操作
+        front = (front + 1) % data.length;
+        size --;
+        if(size == getCapacity() / 4 && getCapacity() / 2 != 0)
+            resize(getCapacity() / 2);
         return ret;
     }
 
     @Override
-    public E getFront() {
-            if(isEmpty())
-                throw new IllegalArgumentException("Queue is empty");
+    public E getFront(){
+        if(isEmpty())
+            throw new IllegalArgumentException("Queue is empty.");
         return data[front];
     }
 
     private void resize(int newCapacity){
 
-        E[] newData = (E[]) new Object[newCapacity+1];
-        for (int i = 0; i < size; i++) {
+        E[] newData = (E[])new Object[newCapacity + 1];
+        for(int i = 0 ; i < size ; i ++)
             newData[i] = data[(i + front) % data.length];
+
         data = newData;
         front = 0;
         tail = size;
-        }
     }
 
     @Override
-    public String toString() {
+    public String toString(){
 
         StringBuilder res = new StringBuilder();
         res.append(String.format("Queue: size = %d , capacity = %d\n", size, getCapacity()));
