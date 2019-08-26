9828a5409cef7f2a92e93ec43b75311f4bc66113
LGSKOKO <420422844@qq.com>
2019-08-26 18:39:52 +0800

    BST(二分搜索树),新添加了增加元素功能

--- BST/src/main/java/BST.java
+++ BST/src/main/java/BST.java
@@ -28,12 +28,45 @@ public class BST<E extends Comparable<E>> {
         root = null;
         size = 0;
     }
-
+    //获取元素个数
     public int getSize(){
         return size;
     }
-
+    //判断是否为空
     public boolean isEmpty(){
         return size == 0;
     }
+
+    //向二分搜索树种添加新的元素e
+    public void add(E e){
+        if(root == null){
+            root = new Node(e);
+             size++;
+        }else
+            add(root,e);
+    }
+
+    //向以root为根的二分搜索树中添加新的元素e，递归算法
+    private void add(Node node,E e){
+        //先对root（根节点）进行一系列判断和相应操作
+        if(node.e == e)
+            return;
+        else if(e.compareTo(node.e)<0 && node.left==null){//因为元素的e类型不是基础类型 所以不能直接进行比较
+            //因为我们该类的E继承自Comparable接口 所以可以直接调用compareTo方法进行比较大小
+            node.left = new Node(e);
+            size++;
+            return;
+        }
+        else if(e.compareTo(node.e)>0 && node.right==null){
+            node.right = new Node(e);
+            size++;
+            return;
+        }
+        //调用递归函数
+        if(e.compareTo(node.e)<0)
+            add(node.left,e);
+        else //e.compareTo(node.e)>0
+            add(node.right,e);
+    }
+
 }//end class BST
